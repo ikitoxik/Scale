dd3a6d67a355fcb7a14d8e6fa7f780a8cc4bdfb9
Nikita Butyrin <94044348+ikitoxik@users.noreply.github.com>
2022-03-24 21:10:33 +0300

    Add files via upload

---
+++ ScaleComp.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Comp
+{
+    partial class ScaleComp
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 134);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(194, 37);
+            this.panel1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(63, 3);
+            this.richTextBox1.MaxLength = 100;
+            this.richTextBox1.MinimumSize = new System.Drawing.Size(30, 10);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(66, 19);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(194, 134);
+            this.panel2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(156, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // ScaleComp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(196, 173);
+            this.Name = "ScaleComp";
+            this.Size = new System.Drawing.Size(194, 171);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ ScaleComp.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Comp
+{
+    public partial class ScaleComp : UserControl
+    {
+        private string unit;   //Единицы измерения 
+        private int imin = 0;   //Минимальное значение шкалы
+        //private double fmin = 0.0;
+       // private double fmax = 0.0;
+        private int imax = 10;   //Максимальное значение
+        private int istep = 1;   //шаг
+        private int current_data = 0;   //Текущее значение
+        private Color scale = Color.DarkRed;   //Цвет шкалы
+        private Color arrow = Color.Red;   //Цвет стрелки
+        private Color bg_scale = Color.Red;   //Цвет круга шкалы
+        private float radius = 0.0f;   //Радиус
+        private float ratio = 0.0f;   //Коэфф для вычисления
+        private int rotation = 180;   //Коэфф для вычисления
+        private float current_ratio = 0f;   //Текущее значение угла
+        private int data_on_scale = 0;   //Текущее значение шкалы
+        //private string min_str = "0";
+       // private string max_str = "10";
+       // private bool float_flag = false;
+        public Color LowerColor
+        {
+            get
+            {
+                return panel1.BackColor;
+            }
+            set
+            {
+                panel1.BackColor = value;
+            }
+        }
+
+        public Color UpperColor
+        {
+            get
+            {
+                return panel2.BackColor;
+            }
+            set
+            {
+                panel2.BackColor = value;
+
+                label1.BackColor = panel2.BackColor;
+                label2.BackColor = panel2.BackColor;
+            }
+        }
+
+        public Color TextPanelColor
+        {
+            get
+            {
+                return richTextBox1.BackColor;
+            }
+            set
+            {
+                richTextBox1.BackColor = value;
+            }
+        }
+
+        public Color TextColor
+        {
+            get
+            {
+                return richTextBox1.ForeColor;
+            }
+            set
+            {
+                richTextBox1.ForeColor = value;
+            }
+        }
+
+        public string Measurement
+        {
+            get
+            {
+                return unit;
+            }
+            set
+            {
+                unit = value;
+            }
+        }
+
+    /*    public string MinScale
+        {
+            get
+            {
+                return min_str;
+            }
+            set
+            {
+                min_str = value;
+                if (!float_flag)
+                {
+                    if (int.TryParse(min_str, out imin))
+                        float_flag = true;
+                    else fmin = Convert.ToDouble(min_str);
+                }
+                else fmin = Convert.ToDouble(min_str);
+                label1.Text = min_str;
+            }
+        }
+        */
+        public int MaxScale
+        {
+            get
+            {
+                return imax;
+            }
+            set
+            {
+                imax = value;
+                label2.Text =Convert.ToString(imax);
+            }
+        }
+
+        public int MinScale
+        {
+            get
+            {
+                return imin;
+            }
+            set
+            {
+                imin = value;
+                label1.Text = Convert.ToString(imin);
+            }
+        }
+        /* public string MaxScale
+          {
+              get
+              {
+                  return max_str;
+              }
+              set
+              {
+                  max_str = value;
+                  if (!float_flag)
+                  {
+                      if (int.TryParse(max_str, out imax))
+                          float_flag = true;
+                      else fmax = Convert.ToDouble(max_str);
+                  }
+                  else fmax = Convert.ToDouble(max_str);
+                  label2.Text = max_str;
+              }
+          }*/
+
+        public int Step
+        {
+            get
+            {
+                return istep;
+            }
+            set
+            {
+                istep = value;
+            }
+        }
+
+        public Color ScaleColor
+        {
+            get
+            {
+                return scale;
+            }
+            set
+            {
+                scale = value;
+            }
+        }
+
+        public Color ArrowColor
+        {
+            get
+            {
+                return arrow;
+            }
+            set
+            {
+                arrow = value;
+            }
+        }
+
+        public Color BackGroundScaleColor
+        {
+            get
+            {
+                return bg_scale;
+            }
+            set
+            {
+                bg_scale = value;
+            }
+
+        }
+
+        public ScaleComp()
+        {
+            InitializeComponent();
+
+            timer1 = new Timer();
+            timer1.Interval = 500;
+            timer1.Tick += new EventHandler(Timer1_Tick);
+            richTextBox1.Text = Convert.ToString(current_data) + " " + unit;
+            label1.Text = Convert.ToString(imin);
+            label2.Text = Convert.ToString(imax);
+            label1.Location = new System.Drawing.Point(label1.Width - 10, panel2.Height - 15);
+            label2.Location = new System.Drawing.Point(panel2.Width - label2.Width, panel2.Height - 15);
+        }
+        //Очистка панели
+        private void draw_panel_clear(Color uppercolor)
+        {
+            using (Graphics g = panel2.CreateGraphics())
+            {
+                g.Clear(uppercolor);
+            }
+        }
+
+        //Рисование шкалы
+        private void draw_panel_Paint()
+        {
+            using (Graphics g = panel2.CreateGraphics())
+            {
+
+                int i = 180;
+               
+                radius = (panel2.Width - 40) / 2;
+                ratio = 180 / ((imax - imin) / istep);
+                
+               
+
+                Pen pen_color = new Pen(scale, 2);
+                Pen pen_color_bg = new Pen(bg_scale, 1);
+
+                g.DrawLine((pen_color), 30, panel2.Height - 10, 20, panel2.Height - 10);
+                g.DrawLine((pen_color), panel2.Width - 20, panel2.Height - 10, panel2.Width - 30, panel2.Height - 10);
+
+                g.DrawRectangle(pen_color, (int)radius + 20, panel2.Height - 10 - (int)radius, 1, 1);
+
+                int x = 20;
+                int y = panel2.Height - 10 - (int)radius;
+                int width = 2 * (int)radius;
+                int height = 2 * (int)radius;
+
+                // Create start and sweep angles on ellipse.
+                int startAngle = 180;
+                int sweepAngle = 180;
+
+                // Draw arc to screen.
+                g.DrawArc(pen_color_bg, x, y, width, height, startAngle, sweepAngle);
+
+                //Рисование точек
+                for (i = 180 + (int)ratio; i < 360; i += (int)ratio)
+                {
+                    double rad = (double)i / 180 * 3.14;
+                    double _x = radius * Math.Cos(rad) + 20 + radius;
+                    double _y = radius * Math.Sin(rad) + panel2.Height - 10;
+
+                    double __x = (radius - 5) * Math.Cos(rad) + 20 + radius;
+                    double __y = (radius - 5) * Math.Sin(rad) + panel2.Height - 10;
+
+                     g.DrawLine((pen_color), (float)_x, (float)_y, (float)__x, (float)__y);
+                    //g.DrawRectangle(pen_color, (int)_x, (int)_y, 1, 1);
+                }
+            }
+        }
+
+        //Добавление шкалы и обновление лейблов
+        private void draw_scale()
+        {
+            draw_panel_clear(panel2.BackColor);
+            label1.BackColor = panel2.BackColor;
+            label2.BackColor = panel2.BackColor;
+            draw_panel_Paint();
+        }
+
+        private int draw_arrow(int min, int current_rotation, int cur_data, float rat)
+        {
+            if (cur_data >= data_on_scale) //cur_data -поступившее значение  
+            {
+                using (Graphics g = panel2.CreateGraphics())
+                {
+                    Pen arrow_color = new Pen(arrow, 2);
+                    Pen bg_color = new Pen(panel2.BackColor, 2);
+
+                    double rad = (double)current_rotation / 180 * 3.14;
+                    double prev_rad = ((double)current_rotation- current_ratio) / 180 * 3.14;
+
+                    double __x = (radius - 10) * Math.Cos(rad) + 20 + radius;
+                    double __y = (radius - 10) * Math.Sin(rad) + panel2.Height - 10;
+               
+                    double _x = (radius - 10) * Math.Cos(prev_rad) + 20 + radius;
+                    double _y = (radius - 10) * Math.Sin(prev_rad) + panel2.Height - 10;
+                    
+
+                    g.DrawLine(bg_color, (int)_x, (int)_y, 20 + radius, panel2.Height - 10);
+                    g.DrawLine(arrow_color, (int)__x, (int)__y, 20 + radius, panel2.Height - 10);
+
+                    richTextBox1.Text = Convert.ToString(data_on_scale) + " " + unit;
+
+                    data_on_scale ++;                    
+                }
+            }
+            return (current_rotation + (int)current_ratio);
+        }
+        //Событие на тике 
+        private void Timer1_Tick(object Sender, EventArgs e)
+        {
+            rotation = draw_arrow(imin, rotation, current_data, current_ratio);
+        }
+
+        //Процедура для старта отображения 
+        public void GetData(int data)
+        {
+            rotation = 180;
+            data_on_scale = imin;
+            current_data = data;
+            draw_scale();
+            current_ratio = 180 / (imax - imin);
+       
+            timer1.Enabled = true;
+
+        }   
+    }
+}
